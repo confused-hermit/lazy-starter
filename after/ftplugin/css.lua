c0624671cc66dc6fd0e57ffa0f0c4f3286f0554b
Aniruddha Kibey <hermit.confused@gmail.com>
2023-07-23 17:46:22 +0530

    added css keybinding

---
+++ after/ftplugin/css.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", "<leader>lc", ":CccPick<cr>", { desc = "ccc color picker" })
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,28 +1,43 @@
-return{
-    {"echasnovski/mini.pairs",
-config = function ()
-    require("mini.pairs").setup({})
-end
-},
-    {"echasnovski/mini.indentscope",
-config = function ()
-    require("mini.indentscope").setup({})
-end
-},
-    {"echasnovski/mini.comment",
-config = function ()
-    require("mini.comment").setup({})
-end
-},
-    {"echasnovski/mini.starter",
-config = function ()
-    require("mini.starter").setup({})
-end
-},
-    {"echasnovski/mini.surround",
-config = function ()
-    require("mini.surround").setup({})
-end
-},
-
+return {
+  {
+    "echasnovski/mini.pairs",
+    config = function()
+      require("mini.pairs").setup({})
+    end,
+  },
+  {
+    "echasnovski/mini.indentscope",
+    config = function()
+      require("mini.indentscope").setup({})
+    end,
+  },
+  {
+    "echasnovski/mini.comment",
+    config = function()
+      require("mini.comment").setup({})
+    end,
+  },
+  {
+    "echasnovski/mini.starter",
+    config = function()
+      require("mini.starter").setup({})
+    end,
+  },
+  {
+    "echasnovski/mini.surround",
+    config = function()
+      require("mini.surround").setup({})
+    end,
+    opts = {
+      mappings = {
+        add = "sa",
+        delete = "sd",
+        find = "sf",
+        find_left = "sF",
+        highlight = "sh",
+        replace = "sr",
+        update_n_lines = "sn",
+      },
+    },
+  },
 }
